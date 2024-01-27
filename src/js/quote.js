75da388277ca15abce1e7fafe4bae12a4b37e9f4
Igor Kushneruk <71212927+igor-bro@users.noreply.github.com>
2024-01-27 22:08:23 +0100

    More optimisations

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   const QUOTE_API_URL = 'https://energyflow.b.goit.study/api/quote';
-  const QUOTE_INTERVAL_MS = 4 * 60 * 60 * 1000; // Кожні 4 години
+  const QUOTE_INTERVAL_MS = 1 * 60 * 60 * 1000; // Кожну годину
+  const LOCAL_STORAGE_KEY = 'quoteData';
 
   const quoteTextElement = document.querySelector('.quote-text');
   const quoteAuthorElement = document.querySelector('.quote-author');
 
   function getStoredQuote() {
-    return JSON.parse(localStorage.getItem('quoteData')) || {};
+    const storedData =
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+    storedData.date = new Date(storedData.date);
+    return storedData;
   }
 
   function saveQuoteToLocalStorage(quote, author, date) {
     localStorage.setItem(
-      'quoteData',
-      JSON.stringify({ quote, author, date: new Date().toDateString() })
+      LOCAL_STORAGE_KEY,
+      JSON.stringify({ quote, author, date: new Date().getTime() })
     );
   }
 
@@ -21,32 +25,30 @@ document.addEventListener('DOMContentLoaded', function () {
     quoteAuthorElement.textContent = author || 'Unknown author';
   }
 
+  function isQuoteChanged(data, storedData) {
+    return (
+      data.quote !== storedData.quote ||
+      data.author !== storedData.author ||
+      !storedData.date ||
+      new Date().toDateString() !== storedData.date
+    );
+  }
+
   async function fetchQuote() {
     try {
       const response = await fetch(QUOTE_API_URL);
       const data = await response.json();
+      const storedData = getStoredQuote();
 
-      const storedQuoteData = getStoredQuote();
-
-      if (!data.quote) {
-        // Якщо цитата не отримана від сервера, використовуємо ту, що вже є в localStorage
+      if (!data.quote || !isQuoteChanged(data, storedData)) {
         displayStoredQuote();
-      } else if (
-        data.quote !== storedQuoteData.quote ||
-        data.author !== storedQuoteData.author ||
-        !storedQuoteData.date ||
-        new Date().toDateString() !== storedQuoteData.date
-      ) {
-        // Зберігаємо нову цитату та автора, відображаємо на сторінці
+      } else {
         saveQuoteToLocalStorage(
           data.quote,
           data.author,
           new Date().toDateString()
         );
         displayQuote(data.quote, data.author);
-      } else {
-        // Якщо цитата не змінилась, відображаємо збережену цитату
-        displayStoredQuote();
       }
     } catch (error) {
       console.error('Error fetching quote:', error);
@@ -54,8 +56,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function displayStoredQuote() {
-    const storedQuoteData = getStoredQuote();
-    displayQuote(storedQuoteData.quote, storedQuoteData.author);
+    const storedData = getStoredQuote();
+    displayQuote(storedData.quote, storedData.author);
   }
 
   setInterval(async () => {
