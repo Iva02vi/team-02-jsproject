f27451528d2393e68287140fdfaf3cebc931e21a
Iva02vi <139965283+Iva02vi@users.noreply.github.com>
2024-01-28 11:30:44 +0000

    Deploying to gh-pages from @ Iva02vi/team-02-jsproject@e0d40fd2fca73b36373b1093ca035e16d38097a0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/quote-ba67f11f.js";const n=document.querySelector(".header-burger"),e=document.querySelector(".mobile-menu-wrapper"),o=document.querySelector(".mobile-menu-close-btn");function t(){e.classList.remove("visually-hidden")}function l(){e.classList.add("visually-hidden")}n.addEventListener("click",t);o.addEventListener("click",l);
+import"./assets/quote-1da8f9b3.js";const n=document.querySelector(".header-burger"),e=document.querySelector(".mobile-menu-wrapper"),o=document.querySelector(".mobile-menu-close-btn");function t(){e.classList.remove("visually-hidden")}function l(){e.classList.add("visually-hidden")}n.addEventListener("click",t);o.addEventListener("click",l);
 //# sourceMappingURL=commonHelpers.js.map
