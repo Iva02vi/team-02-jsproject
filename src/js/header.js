b3bf459d78ebaab1339ddcf8084d859da33364aa
Iva02vi <ivanova.valery.02@gmail.com>
2024-01-30 00:08:44 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -9,13 +9,13 @@ function goToPage(path) {
 }
 
 const path = location.pathname;
-console.log(path);
-if (path === '/team-02-jsproject/index.html' || path === '/team-02-jsproject/') {
+
+if (path === '/team-02-jsproject/index.html' || path === '/team-02-jsproject/' || path === '/index.html' || path === '/') {
   secondChild.classList.remove('is-active');
   mobSecondChild.classList.remove('is-active');
   firstChild.classList.add('is-active');
   mobFirstChild.classList.add('is-active');
-} else if (path === '/team-02-jsproject/favorites.html') {
+} else if (path === '/team-02-jsproject/favorites.html' || path === '/favorites.html') {
   firstChild.classList.remove('is-active');
   mobFirstChild.classList.remove('is-active');
   secondChild.classList.add('is-active');
