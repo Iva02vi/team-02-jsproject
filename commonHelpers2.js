9c4eac99aa856d90bc2e6ca312e7cb1b01532680
Iva02vi <139965283+Iva02vi@users.noreply.github.com>
2024-01-30 22:04:07 +0000

    Deploying to gh-pages from @ Iva02vi/team-02-jsproject@5096842f3f9d3c53f130d1f4eb739b0a79527363 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{o as R}from"./assets/modal-window-exercise-f04ed7ab.js";/* empty css                      */import{i as S,a as q}from"./assets/vendor-8cce9181.js";const k=document.querySelector(".back-to-top"),C=()=>{const e=Math.max(document.documentElement.scrollTop,document.body.scrollTop);e>0&&(window.requestAnimationFrame(C),window.scrollTo(0,e-e/8))};k.addEventListener("click",C);window.addEventListener("scroll",()=>{(window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop)>350?k.classList.add("active"):k.classList.remove("active")});var j=document.getElementById("myAudio");j.addEventListener("ended",function(){S.show({title:"Lets do it!",message:"Subscribe in the bottom, and learn about new exercises!",position:"bottomCenter",timeout:1e4,backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"})});const y=document.querySelector(".exercises-gallery-label"),i=document.querySelector(".exercises-gallery-group"),p=document.querySelector(".exercises-gallery-filter"),g=document.querySelector(".page-buttons-container");let l=null;const f=document.createElement("span"),$=document.querySelector("#filtre-key"),b=document.querySelector(".search-tool"),D=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location),M=document.querySelector(".error-card-message"),O="https://energyflow.b.goit.study/api/";let u=1;const c={content:null,title:null},W="https://energyflow.b.goit.study/api/filters";let a={filter:"Muscles",page:1,limit:8},m="filter";const A="muscles",I=q.create({baseURL:W});function B(e){i.innerHTML="";const t=e.results.map(r=>`<li class="exercises-item-background">
+import"./assets/modal-window-exercise-b86e61c1.js";/* empty css                      */import{i as S,a as T}from"./assets/vendor-8cce9181.js";const w=document.querySelector(".back-to-top"),q=()=>{const e=Math.max(document.documentElement.scrollTop,document.body.scrollTop);e>0&&(window.requestAnimationFrame(q),window.scrollTo(0,e-e/8))};w.addEventListener("click",q);window.addEventListener("scroll",()=>{(window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop)>350?w.classList.add("active"):w.classList.remove("active")});var W=document.getElementById("myAudio");W.addEventListener("ended",function(){S.show({title:"Lets do it!",message:"Subscribe in the bottom, and learn about new exercises!",position:"bottomCenter",timeout:1e4,backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"})});const f=document.querySelector(".exercises-gallery-label"),c=document.querySelector(".exercises-gallery-group"),d=document.querySelector(".exercises-gallery-filter"),y=document.querySelector(".page-buttons-container");let i=null;const g=document.createElement("span"),C=document.querySelector("#filtre-key"),h=document.querySelector(".search-tool");new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location);const $=document.querySelector(".error-card-message"),D="https://energyflow.b.goit.study/api/";let u=1;const l={content:null,title:null},N="https://energyflow.b.goit.study/api/filters";let s={filter:"Muscles",page:1,limit:8},m="filter";const M="muscles",O=T.create({baseURL:N});function A(e){c.innerHTML="";const t=e.results.map(r=>`<li class="exercises-item-background">
       <a href="${r.imgUrl}">
       <div>
         <img
@@ -11,46 +11,49 @@ import{o as R}from"./assets/modal-window-exercise-f04ed7ab.js";/* empty css     
           </div>
           </div>
       </a>
-    </li>`);return i.insertAdjacentHTML("beforeend",t.join("")),e}p.addEventListener("click",function(e){if(P(),h(),b.style.display="none",e.target.tagName==="BUTTON"){m="filter";const t=e.target.getAttribute("name");E(t)}});function E(e){b.style.display="none";let t={};switch(F(),e){case"body":a.filter="Body parts",t=p.querySelectorAll(`button[name="${e}"]`);break;case"equipment":a.filter="Equipment",t=p.querySelectorAll(`button[name="${e}"]`);break;default:a.filter="Muscles",t=p.querySelectorAll(`button[name="${e}"]`)}Q(t);const s=`?${new URLSearchParams(a).toString()}`;U(s).then(o=>B(o)).then(o=>L(o.totalPages)).catch(o=>console.error(o))}function N(){const t=`?${new URLSearchParams(a).toString()}`;U(t).then(r=>B(r)).catch(r=>console.error(r))}async function U(e){try{return(await I.get(e)).data}catch(t){console.error(t)}}function Q(e){const t=p.querySelectorAll("button"),r=Array.from(e);for(let s=0;s<t.length;s++){const o=t[s];r.includes(o)?(o.style.backgroundColor="var(--dark-gray)",o.style.color="var(--white)"):(o.style.backgroundColor="var(--white-smoke)",o.style.color="var(--black)")}console.log("changeButtonColor function toggle is: "+m)}function L(e){let t="";g.innerHTML="";for(let r=1;r<=e;r++)r===1?t+=`<li>
+    </li>`);return c.insertAdjacentHTML("beforeend",t.join("")),e}d.addEventListener("click",function(e){if(H(),b(),workoutCountPages=0,h.style.display="none",e.target.tagName==="BUTTON"){m="filter";const t=e.target.getAttribute("name");L(t)}});function L(e){h.style.display="none";let t={};switch(U(),e){case"body":s.filter="Body parts",t=d.querySelectorAll(`button[name="${e}"]`);break;case"equipment":s.filter="Equipment",t=d.querySelectorAll(`button[name="${e}"]`);break;default:s.filter="Muscles",t=d.querySelectorAll(`button[name="${e}"]`)}z(t);const o=`?${new URLSearchParams(s).toString()}`;B(o).then(n=>A(n)).then(n=>k(n.totalPages)).catch(n=>console.error(n))}function I(){const t=`?${new URLSearchParams(s).toString()}`;B(t).then(r=>A(r)).catch(r=>console.error(r))}async function B(e){try{return(await O.get(e)).data}catch(t){console.error(t)}}function z(e){const t=d.querySelectorAll("button"),r=Array.from(e);for(let o=0;o<t.length;o++){const n=t[o];r.includes(n)?(n.style.backgroundColor="var(--dark-gray)",n.style.color="var(--white)"):(n.style.backgroundColor="var(--white-smoke)",n.style.color="var(--black)")}console.log("changeButtonColor function toggle is: "+m)}function k(e){let t="";y.innerHTML="";for(let r=1;r<=e;r++)r===1?t+=`<li>
       <button class="page-button active" type="button" id="${r}">${r}</button>
     </li>`:t+=`<li>
-      <button class="page-button" type="button" id="${r}">${r}</button>`;g.insertAdjacentHTML("afterbegin",t)}g.addEventListener("click",function(e){const t=e.target.closest(".page-button");if(t){const r=t.id;H(r)}});function H(e){document.querySelectorAll(".page-button").forEach(r=>{if(r.id===e)switch(r.classList.add("active"),m){case"filter":a.page=e,N();break;case"workout":u=e;const s={limit:x(),page:u};s[c.content]=c.title,T(s);break}else r.classList.remove("active")})}window.addEventListener("resize",e=>{e.preventDefault(),F(),E(A),H(1)});function F(){document.documentElement.clientWidth>1440?(a.page=1,a.limit=12):(a.page=1,a.limit=8)}E(A);i.addEventListener("click",e=>{e.preventDefault();const t=e.target.closest(".exercises-item-background");if(t){m="workout",b.style.display="flex";const r=t.querySelector(".name-card").textContent;let s=t.querySelector(".type-card").textContent.toLowerCase().replace(/\s/g,"");s==="bodyparts"&&(s=s.slice(0,-1)),z(r,s)}});function V(){h(),P(),i.innerHTML="",g.innerHTML=""}function Y(e){let t="";e=Math.floor(e);for(let r=0;r<e;r++)t+=`
+      <button class="page-button" type="button" id="${r}">${r}</button>`;y.insertAdjacentHTML("afterbegin",t)}y.addEventListener("click",function(e){const t=e.target.closest(".page-button");if(t){const r=t.id;P(r)}});function P(e){document.querySelectorAll(".page-button").forEach(r=>{if(r.id===e)switch(r.classList.add("active"),m){case"filter":s.page=e,I();break;case"workout":u=e;const o={limit:E(),page:u};o[l.content]=l.title,x(o);break}else r.classList.remove("active")})}window.addEventListener("resize",e=>{e.preventDefault(),U(),L(M),P(1)});function U(){document.documentElement.clientWidth>1440?(s.page=1,s.limit=12):(s.page=1,s.limit=8)}L(M);c.addEventListener("click",e=>{e.preventDefault();const t=e.target.closest(".exercises-item-background");if(t){m="workout",h.style.display="flex";const r=t.querySelector(".name-card").textContent;let o=t.querySelector(".type-card").textContent.toLowerCase().replace(/\s/g,"");if(o==="bodyparts"&&(o=o.slice(0,-1)),G(r,o),workoutCountPages===0)return;workoutCountPages>=3?k(3):k(workoutCountPages)}});function Q(){b(),H(),c.innerHTML="",y.innerHTML=""}function V(e){let t="";return t+=`
         <span class="rating-star-icon">
             <svg class="rating-star" width="18" height="18" aria-label="rating-star">
                    <use href="./img/sprite.svg#icon-Star-1"></use>
             </svg>
-        </span>`;return t}b.addEventListener("click",e=>{if(e.preventDefault(),e.target.localName!="svg")return;u=1;const t=$.value.trim();if(console.log(t),t.length===0){S.error({title:"Error",position:"topCenter",message:"Sorry, Please choose an exercise."});return}V();const r={limit:x(),page:u,keyword:t};r[c.content]=c.title,$.value="",T(r)});function x(){return document.documentElement.clientWidth>768?12:8}function _(){M.style.display="block"}function h(){M.style.display="none"}h();function z(e,t){c.title=e,c.content=t,u=1;const r={limit:x(),page:u};r[c.content]=e,G(e),T(r)}async function T(e){try{const t=await q.get(`${O}exercises`,{params:e}),{totalPages:r,results:s}=t.data;if(s.length==0){_();return}let o=s.reduce((d,n)=>{i.innerHTML="",g.innerHTML="";const v=Y(n.rating);return d+`<li class="gallery-list-item">
-                <div class="workout-box">
-                    <div class="workout-rating">
-                        <p class="workout-title">WORKOUT</p>
-                        <p class="rating-title">${n.rating}
-                          ${v}
-                        </p>
-            <button type="button" class="start-button" data-exercise-id=${n._id} >Start
-                            <span class="arrow-icon">
-                                <svg class="start-arrow-icon" width="16" height="16" aria-label="start-arrow">
-                                    <use href=${D}></use>
+        </span>`,t}h.addEventListener("click",e=>{if(e.preventDefault(),e.target.localName!="svg")return;u=1;const t=C.value.trim();if(t.length===0){S.error({title:"Error",position:"topCenter",message:"Sorry, Please choose an exercise."});return}Q();const r={limit:E(),page:u,keyword:t};r[l.content]=l.title,C.value="",x(r)});function E(){return document.documentElement.clientWidth>768?12:8}function Y(){$.style.display="block"}function b(){$.style.display="none"}b();function G(e,t){l.title=e,l.content=t,u=1;const r={limit:E(),page:u};r[l.content]=e,K(e),x(r)}function K(e){i=document.createElement("span"),i.classList.add("exercise-title-card"),i.innerHTML=e,g.classList.add("exercises-gallery-label"),g.innerHTML=" / ",f.appendChild(g),f.appendChild(i)}function H(){i&&(f.removeChild(i),f.removeChild(g),i=null,b())}async function x(e){try{const t=await T.get(`${D}exercises`,{params:e}),{totalPages:r,results:o}=t.data;if(o.length==0){c.innerHTML="",Y();return}let n=o.reduce((p,a)=>{const j=V(a.rating),F=Number(a.rating).toFixed(1),R=_(a.name);return p+`<li class="gallery-item-list">
+                <div class="workout-header-wrap">
+                        <div class= "workout-and-rating">
+                            <p class="workout-item-title">WORKOUT</p>
+                            <p class="rating-title-item">${F}
+                                ${j}
+                            </p>
+                            </div>
+                        <div class="start-button-wrap">
+                            <button type="button" class="start-button-item">Start
+                                <svg class="start-workout-icon" width="14" height="14" aria-label="start-arrow">
+                                    <use href="./img/sprite.svg#icon-arrow"></use>
                                 </svg>
-                            </span>
                             </button>
+                        </div>
                     </div>
-                    <div class="workout-type">
-                        <svg class="run-man-icon" width="24" height="24" aria-label="run-man">
-                            <use href="../img/sprite.svg#icon-lighticon"></use>
+                       <div class="workout-type-item">
+                        <svg  width="24" height="24" aria-label="run-man">
+                            <use href="./img/sprite.svg#icon-lighticon"></use>
                         </svg>
-                        <p class="workout-name">${n.name}</p>
+                        <p class="workout-name-item">${R}</p>
                     </div>
-                    <div class="body-description">
-                        <p class="burned-callories">Burned calories:
-                            <span class="amount-callories">${n.burnedCalories}/${n.time} min</span>
+                <div class="workout-items-box">
+                
+                 
+                    <div class="workout-description">
+                        <p class="description-item-name">Burned calories:
+                            <span class="description-item-value">${a.burnedCalories} / ${a.time} min</span>
                         </p>
-                        <p class="filtred-class">Filtre:
-                            <span class="filter-type">${c.content}</span>
+                        <p class="description-item-name">Body part:
+                            <span class="description-item-value">${a.bodyPart}</span>
                         </p>
-                        <p class="target">Target:
-                        <span class="key-word">${n.target}</span>
+                        <p class="description-item-name">Target:
+                            <span class="description-item-value">${a.target}</span>
                         </p>
-                    </div>
-                </div>
-            </li>`},"");i.insertAdjacentHTML("beforeend",o),i.addEventListener("click",async d=>{let n;const v=d.target;d.target&&d.target.closest(".start-button")&&(n=v.closest(".start-button").getAttribute("data-exercise-id"),await R(n))}),m="workout",r<3?L(r):L(3)}catch(t){console.log(t)}}function G(e){l=document.createElement("span"),l.classList.add("exercise-title-card"),l.innerHTML=e,f.classList.add("exercises-gallery-label"),f.innerHTML=" / ",y.appendChild(f),y.appendChild(l)}function P(){l&&(y.removeChild(l),y.removeChild(f),l=null,h())}const K="feedback-form",w=document.querySelector(".footer-modal-form");w.addEventListener("submit",e=>{e.preventDefault();const{email:t}=w.elements,r={email:t.value.trim()};S.success({position:"topCenter",message:"We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being.",backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"}),console.log(r),localStorage.removeItem(K),w.reset()});
+                    </div> 
+            </li>`},"");workoutCountPages=t.data.totalPages,c.innerHTML="",c.insertAdjacentHTML("beforeend",n),m="workout"}catch(t){console.log(t)}}function _(e){const t=document.documentElement.clientWidth;let r=20,o=295,n=.75;t>1440?(r=24,o=424,n=.85):t>768&&(r=24,o=313,n=.8);const p=o/(r/2)*n;return e.length>p?e.slice(0,p)+"...":e}const J="feedback-form",v=document.querySelector(".footer-modal-form");v.addEventListener("submit",e=>{e.preventDefault();const{email:t}=v.elements,r={email:t.value.trim()};S.success({position:"topCenter",message:"We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being.",backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"}),console.log(r),localStorage.removeItem(J),v.reset()});
 //# sourceMappingURL=commonHelpers2.js.map
