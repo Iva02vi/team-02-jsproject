ba5aa2f9c71a7327970c0d448a68b9a4acd903f1
Iva02vi <139965283+Iva02vi@users.noreply.github.com>
2024-02-01 02:14:30 +0000

    Deploying to gh-pages from @ Iva02vi/team-02-jsproject@722933099563f32d69de10f56d1c604d10548ccb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,47 +1,45 @@
-import{o as S}from"./assets/mob-menu-fc4d99a8.js";import"./assets/vendor-8cce9181.js";const w=document.querySelector(".favorites-page-items-not-found"),l=document.querySelector(".favorites-page-items-gallery"),y=document.querySelector(".favorites-page-items"),r=document.querySelector(".favorites-mobile-pagination"),k=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location),p="favorites",$=localStorage.getItem(p),g=JSON.parse($),x=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-lighticon",self.location),E=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-trash",self.location),n=8;let d=1,o=d*n,c=o-n,a="",f,v,b,m=Math.ceil(g.length/n);function h(t){t.style.display="none",t===l&&innerWidth>768&&(y.style.paddingRight="48px")}function L(t){t.style.display="flex",t===l&&innerWidth>768&&(y.style.paddingRight="0")}window.innerWidth<768&&L(r);function u(t){l.innerHTML="";const i=t.reduce((e,s)=>e+`<li class="gallery-list-item">
-                <div class="workout-box">
-                    <div class="workout-header">
-                        <div class="workout-header-wrap">
-                            <span class="workout-title">WORKOUT</span>
-                            <button type="button" class="delete-workout-btn" id="${s._id}">                            
-                                <svg class="trash-icon" id="${s._id}" width="16" height="16" aria-label="trash-icon">
-                                  <use href=${E} id="${s._id}"></use>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="start-button-wrap">
-                            <button type="button" class="start-button-item" id=${s._id}>Start
-                                <svg class="start-arrow-icon" id=${s._id} width="14" height="14" aria-label="start-arrow">
-                                  <use href=${k}></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="workout-type">
-                        <svg class="run-man-icon" width="24" height="24" aria-label="run-man">
-                            <use href="${x}"></use>
-                        </svg>
-                        <h3 class="workout-name">${s.name}</h3>
-                    </div>
-                    <div class="workout-description">
-                        <p class="description-item-name">Burned calories:
-                            <span class="description-item-value">${s.burnedCalories} / ${s.time} min</span>
-                        </p>
-                        <p class="description-item-name">Body part:
-                            <span class="description-item-value">${s.bodyPart}</span>
-                        </p>
-                        <p class="description-item-name">Target:
-                            <span class="description-item-value">${s.target}</span>
-                        </p>
-                    </div>
-                </div>
-            </li>`,"");l.innerHTML=i}$===null||g.length===0?h(l):(h(w),u(g));l.addEventListener("click",async t=>{let i;const e=t.target;e&&e.closest(".start-button")&&(i=e.closest(".start-button").getAttribute("id"),await S(i))});if(window.innerWidth<768){a="",r.innerHTML="";for(let t=1;t<=m;t++)t===1?a+=`
+import{o as L}from"./assets/mob-menu-fc4d99a8.js";import"./assets/vendor-8cce9181.js";const w=document.querySelector(".favorites-page-items-not-found"),l=document.querySelector(".favorites-page-items-gallery"),y=document.querySelector(".favorites-page-items"),r=document.querySelector(".favorites-mobile-pagination"),S=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location),g="favorites",$=localStorage.getItem(g),p=JSON.parse($),x=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-lighticon",self.location),E=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-trash",self.location),n=8;let d=1,o=d*n,c=o-n,a="",f,v,b,m=Math.ceil(p.length/n);function h(t){t.style.display="none",t===l&&innerWidth>768&&(y.style.paddingRight="48px")}function k(t){t.style.display="flex",t===l&&innerWidth>768&&(y.style.paddingRight="0")}window.innerWidth<768&&k(r);function u(t){l.innerHTML="";const i=t.reduce((e,s)=>e+`<li class="gallery-list-item">
+        <div class="workout-box" id=${s._id}>
+          <div class="workout-header">
+              <div class="workout-header-wrap">
+                  <span class="workout-title">WORKOUT</span>
+                  <button type="button" class="delete-workout-btn" id="${s._id}">                            
+                      <svg class="trash-icon" id="${s._id}" width="16" height="16" aria-label="trash-icon">
+                        <use href=${E} id="${s._id}"></use>
+                      </svg>
+                  </button>
+              </div>
+              <div class="start-button-wrap">Start
+                  <svg class="start-arrow-icon" id=${s._id} width="14" height="14" aria-label="start-arrow">
+                    <use href=${S}></use>
+                  </svg>
+              </div>
+          </div>
+          <div class="workout-type">
+              <svg class="run-man-icon" width="24" height="24" aria-label="run-man">
+                  <use href="${x}"></use>
+              </svg>
+              <h3 class="workout-name">${s.name}</h3>
+          </div>
+          <div class="workout-description">
+              <p class="description-item-name">Burned calories:
+                  <span class="description-item-value">${s.burnedCalories} / ${s.time} min</span>
+              </p>
+              <p class="description-item-name">Body part:
+                  <span class="description-item-value">${s.bodyPart}</span>
+              </p>
+              <p class="description-item-name">Target:
+                  <span class="description-item-value">${s.target}</span>
+              </p>
+          </div>
+        </div>
+      </li>`,"");l.innerHTML=i}$===null||p.length===0?h(l):(h(w),u(p));l.addEventListener("click",async t=>{let i;const e=t.target;e&&e.closest(".workout-box")&&(i=e.closest(".workout-box").getAttribute("id"),await L(i))});if(window.innerWidth<768){a="",r.innerHTML="";for(let t=1;t<=m;t++)t===1?a+=`
         <li>
           <button class="favorites-pagination-button fav-active-page" type="button" id="${t}">${t}</button>
         </li>`:a+=`
         <li>
           <button class="favorites-pagination-button" type="button" id="${t}">${t}</button>
-        </li>`;r.innerHTML=a,u(g.slice(c,o)),r.addEventListener("click",t=>{if(t.preventDefault(),t.target.classList.value.includes("favorites-pagination-button")){d=t.target.id,o=d*n,c=o-n,f=localStorage.getItem(p),v=JSON.parse(f),u(v.slice(c,o));const i=t.target.closest(".favorites-pagination-button");b=document.querySelectorAll(".favorites-pagination-button"),b.forEach(e=>{e.id===i.id?e.classList.add("fav-active-page"):e.classList.remove("fav-active-page")}),window.scrollBy({top:700,behavior:"smooth"})}})}l.addEventListener("click",t=>{if(t.preventDefault(),t.target.className==="delete-workout-btn"){f=localStorage.getItem(p),v=JSON.parse(f);const i=v.filter(e=>e._id!==t.target.id);if(localStorage.setItem(p,JSON.stringify(i)),i.length===0)h(l),L(w),window.scrollBy({top:700,behavior:"smooth"}),window.innerWidth<768&&h(r);else if(window.innerWidth<768){m=Math.ceil(i.length/n),a="";for(let e=1;e<=m;e++)e===Number(d)?a+=`
+        </li>`;r.innerHTML=a,u(p.slice(c,o)),r.addEventListener("click",t=>{if(t.preventDefault(),t.target.classList.value.includes("favorites-pagination-button")){d=t.target.id,o=d*n,c=o-n,f=localStorage.getItem(g),v=JSON.parse(f),u(v.slice(c,o));const i=t.target.closest(".favorites-pagination-button");b=document.querySelectorAll(".favorites-pagination-button"),b.forEach(e=>{e.id===i.id?e.classList.add("fav-active-page"):e.classList.remove("fav-active-page")}),window.scrollBy({top:700,behavior:"smooth"})}})}l.addEventListener("click",t=>{if(t.preventDefault(),t.target.className==="delete-workout-btn"){f=localStorage.getItem(g),v=JSON.parse(f);const i=v.filter(e=>e._id!==t.target.id);if(localStorage.setItem(g,JSON.stringify(i)),i.length===0)h(l),k(w),window.scrollBy({top:700,behavior:"smooth"}),window.innerWidth<768&&h(r);else if(window.innerWidth<768){m=Math.ceil(i.length/n),a="";for(let e=1;e<=m;e++)e===Number(d)?a+=`
               <li>
                 <button class="favorites-pagination-button fav-active-page" type="button" id="${e}">${e}</button>
               </li>`:a+=`
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,40 +1,39 @@
-import{o as O}from"./assets/mob-menu-fc4d99a8.js";/* empty css                      */import{a as k,i as b}from"./assets/vendor-8cce9181.js";const A=document.querySelector(".loader");k.interceptors.request.use(e=>(A.style.display="inline-block",e),e=>console.error(e));k.interceptors.response.use(e=>(A.style.display="none",e),e=>Promise.reject(e));const T=document.querySelector(".back-to-top"),U=()=>{const e=Math.max(document.documentElement.scrollTop,document.body.scrollTop);e>0&&(window.requestAnimationFrame(U),window.scrollTo(0,e-e/8))};T.addEventListener("click",U);window.addEventListener("scroll",()=>{(window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop)>350?T.classList.add("active"):T.classList.remove("active")});var D=document.getElementById("myAudio");D.addEventListener("ended",function(){b.show({title:"Lets do it!",message:"Subscribe in the bottom, and learn about new exercises!",position:"bottomCenter",timeout:1e4,backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"})});const w=document.querySelector(".exercises-gallery-label"),n=document.querySelector(".exercises-gallery-group"),p=document.querySelector(".exercises-gallery-filter"),g=document.querySelector(".page-buttons-container");let c=null;const h=document.createElement("span"),C=document.querySelector("#filtre-key"),d=document.querySelector(".search-tool"),I=document.querySelector(".icon-search"),N=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location),G=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-Star-1",self.location),z=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-lighticon",self.location),B=document.querySelector(".error-card-message"),Q="https://energyflow.b.goit.study/api/";let v=1;const l={content:null,title:null},V="https://energyflow.b.goit.study/api/filters";let i={filter:"Muscles",page:1,limit:8},m=0,f="filter";const _="muscles",Y=k.create({baseURL:V});p.addEventListener("click",function(e){if(H(),L(),m=0,d.style.display="none",e.target.tagName==="BUTTON"){f="filter";const t=e.target.getAttribute("name");S(),P(t)}});n.addEventListener("click",e=>{e.preventDefault();const t=e.target.closest(".exercises-item-background");if(t){f="workout",d.style.display="flex";const r=t.querySelector(".name-card").textContent;let s=t.querySelector(".type-card").textContent.toLowerCase().replace(/\s/g,"");if(s==="bodyparts"&&(s=s.slice(0,-1)),S(),se(r,s),m===0)return;m>=3?y(3):y(m)}});g.addEventListener("click",function(e){const t=e.target.closest(".page-button");if(t){const r=t.id;X(r)}});function P(e){d.style.display="none";let t={};switch(Z(),e){case"body":i.filter="Body parts",t=p.querySelectorAll(`button[name="${e}"]`);break;case"equipment":i.filter="Equipment",t=p.querySelectorAll(`button[name="${e}"]`);break;default:i.filter="Muscles",t=p.querySelectorAll(`button[name="${e}"]`)}J(t);const s=`?${new URLSearchParams(i).toString()}`;W(s).then(o=>j(o)).then(o=>y(o.totalPages,1)).catch(o=>console.error(o))}function J(e){const t=p.querySelectorAll("button"),r=Array.from(e);for(let s=0;s<t.length;s++){const o=t[s];r.includes(o)?(o.style.backgroundColor="var(--dark-gray)",o.style.color="var(--white)"):(o.style.backgroundColor="var(--white-smoke)",o.style.color="var(--black)")}}function K(){const t=`?${new URLSearchParams(i).toString()}`;W(t).then(r=>j(r)).catch(r=>console.error(r))}async function W(e){try{return(await Y.get(e)).data}catch(t){console.error(t)}}async function $(e){try{const t=await k.get(`${Q}exercises`,{params:e}),{totalPages:r,results:s}=t.data;if(s.length==0){n.innerHTML="",re();return}let o=s.reduce((u,a)=>{n.innerHTML="",g.innerHTML="";const E=te(a.rating),F=Number(a.rating).toFixed(1);return u+`<li class="gallery-item-list" >
-                <div class="workout-header-wrap">
-                        <div class="workout-and-rating">
-                            <p class="workout-item-title">WORKOUT</p>
-                            <p class="rating-title-item">${F}
-                                ${E}
-                            </p>
-                            </div>
-                        <div class="start-button-wrap">
-                            <button type="button" class="start-button-item" data-exercise-id=${a._id}>Start
-                                <svg class="start-workout-icon" width="14" height="14" aria-label="start-arrow">
-                                    <use href=${N}></use>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                       <div class="workout-type-item">
-                        <svg  width="24" height="24" aria-label="run-man">
-                            <use href=${z}></use>
-                        </svg>
-                        <p class="workout-name-item">${a.name}</p>
-                    </div>
-                <div class="workout-items-box">
-                
-                 
-                    <div class="workout-description">
-                        <p class="description-item-name">Burned calories:
-                            <span class="description-item-value">${a.burnedCalories} / ${a.time} min</span>
-                        </p>
-                        <p class="description-item-name">Body part:
-                            <span class="description-item-value">${a.bodyPart}</span>
-                        </p>
-                        <p class="description-item-name">Target:
-                            <span class="description-item-value">${a.target}</span>
-                        </p>
-                    </div> 
-            </li>`},"");m=t.data.totalPages,n.innerHTML="",n.insertAdjacentHTML("beforeend",o),n.addEventListener("click",async u=>{let a;const E=u.target;u.target&&u.target.closest(".start-button-item")&&(a=E.closest(".start-button-item").getAttribute("data-exercise-id"),await O(a))}),f="workout",r<3?y(r,e.page):y(3,e.page)}catch(t){console.log(t)}}function j(e){n.innerHTML="";const t=e.results.map(r=>`<li class="exercises-item-background">
+import{o as O}from"./assets/mob-menu-fc4d99a8.js";/* empty css                      */import{a as k,i as b}from"./assets/vendor-8cce9181.js";const A=document.querySelector(".loader");k.interceptors.request.use(e=>(A.style.display="inline-block",e),e=>console.error(e));k.interceptors.response.use(e=>(A.style.display="none",e),e=>Promise.reject(e));const T=document.querySelector(".back-to-top"),U=()=>{const e=Math.max(document.documentElement.scrollTop,document.body.scrollTop);e>0&&(window.requestAnimationFrame(U),window.scrollTo(0,e-e/8))};T.addEventListener("click",U);window.addEventListener("scroll",()=>{(window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop)>350?T.classList.add("active"):T.classList.remove("active")});var D=document.getElementById("myAudio");D.addEventListener("ended",function(){b.show({title:"Lets do it!",message:"Subscribe in the bottom, and learn about new exercises!",position:"bottomCenter",timeout:1e4,backgroundColor:"var( --dark-gray-hover)",messageColor:"var( --white-smoke)"})});const w=document.querySelector(".exercises-gallery-label"),n=document.querySelector(".exercises-gallery-group"),p=document.querySelector(".exercises-gallery-filter"),g=document.querySelector(".page-buttons-container");let c=null;const h=document.createElement("span"),C=document.querySelector("#filtre-key"),d=document.querySelector(".search-tool"),I=document.querySelector(".icon-search"),N=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-arrow",self.location),G=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-Star-1",self.location),z=new URL("/team-02-jsproject/assets/sprite-7048d1fb.svg#icon-lighticon",self.location),B=document.querySelector(".error-card-message"),Q="https://energyflow.b.goit.study/api/";let v=1;const l={content:null,title:null},V="https://energyflow.b.goit.study/api/filters";let a={filter:"Muscles",page:1,limit:8},m=0,f="filter";const _="muscles",Y=k.create({baseURL:V});p.addEventListener("click",function(e){if(H(),L(),m=0,d.style.display="none",e.target.tagName==="BUTTON"){f="filter";const t=e.target.getAttribute("name");S(),P(t)}});n.addEventListener("click",e=>{e.preventDefault();const t=e.target.closest(".exercises-item-background");if(t){f="workout",d.style.display="flex";const r=t.querySelector(".name-card").textContent;let s=t.querySelector(".type-card").textContent.toLowerCase().replace(/\s/g,"");if(s==="bodyparts"&&(s=s.slice(0,-1)),S(),se(r,s),m===0)return;m>=3?y(3):y(m)}});g.addEventListener("click",function(e){const t=e.target.closest(".page-button");if(t){const r=t.id;X(r)}});function P(e){d.style.display="none";let t={};switch(Z(),e){case"body":a.filter="Body parts",t=p.querySelectorAll(`button[name="${e}"]`);break;case"equipment":a.filter="Equipment",t=p.querySelectorAll(`button[name="${e}"]`);break;default:a.filter="Muscles",t=p.querySelectorAll(`button[name="${e}"]`)}J(t);const s=`?${new URLSearchParams(a).toString()}`;W(s).then(o=>j(o)).then(o=>y(o.totalPages,1)).catch(o=>console.error(o))}function J(e){const t=p.querySelectorAll("button"),r=Array.from(e);for(let s=0;s<t.length;s++){const o=t[s];r.includes(o)?(o.style.backgroundColor="var(--dark-gray)",o.style.color="var(--white)"):(o.style.backgroundColor="var(--white-smoke)",o.style.color="var(--black)")}}function K(){const t=`?${new URLSearchParams(a).toString()}`;W(t).then(r=>j(r)).catch(r=>console.error(r))}async function W(e){try{return(await Y.get(e)).data}catch(t){console.error(t)}}async function $(e){try{const t=await k.get(`${Q}exercises`,{params:e}),{totalPages:r,results:s}=t.data;if(s.length==0){n.innerHTML="",re();return}let o=s.reduce((E,i)=>{n.innerHTML="",g.innerHTML="";const u=te(i.rating),F=Number(i.rating).toFixed(1);return E+`<li class="gallery-item-list">
+          <button type="button" class="gallery-button-item" data-exercise-id=${i._id}>
+              <div class="workout-header-wrap">
+                  <div class="workout-and-rating">
+                    <p class="workout-item-title">WORKOUT</p>
+                    <p class="rating-title-item">${F}
+                        ${u}
+                    </p>
+                  </div>
+                  <div class="start-button-wrap">Start
+                      <svg class="start-workout-icon" width="14" height="14" aria-label="start-arrow">
+                          <use href=${N}></use>
+                      </svg>
+                  </div>
+              </div>
+              <div class="workout-type-item">
+                <svg  width="24" height="24" aria-label="run-man">
+                    <use href=${z}></use>
+                </svg>
+                <p class="workout-name-item">${i.name}</p>
+              </div>
+              <div class="workout-items-box">
+
+              <div class="workout-description">
+                  <p class="description-item-name">Burned calories:
+                      <span class="description-item-value">${i.burnedCalories} / ${i.time} min</span>
+                  </p>
+                  <p class="description-item-name">Body part:
+                      <span class="description-item-value">${i.bodyPart}</span>
+                  </p>
+                  <p class="description-item-name">Target:
+                      <span class="description-item-value">${i.target}</span>
+                  </p>
+              </div> 
+            </button>
+          </li>`},"");m=t.data.totalPages,n.innerHTML="",n.insertAdjacentHTML("beforeend",o),n.addEventListener("click",async E=>{let i;const u=E.target;u&&u.closest(".gallery-button-item")&&(i=u.closest(".gallery-button-item").getAttribute("data-exercise-id"),await O(i))}),f="workout",r<3?y(r,e.page):y(3,e.page)}catch(t){console.log(t)}}function j(e){n.innerHTML="";const t=e.results.map(r=>`<li class="exercises-item-background">
       <a href="${r.imgUrl}">
       <div>
         <img
@@ -50,7 +49,7 @@ import{o as O}from"./assets/mob-menu-fc4d99a8.js";/* empty css                  
     </li>`);return n.insertAdjacentHTML("beforeend",t.join("")),e}function y(e,t){let r="";g.innerHTML="";for(let s=1;s<=e;s++)s==t?r+=`<li>
       <button class="page-button active" type="button" id="${s}">${s}</button>
     </li>`:r+=`<li>
-      <button class="page-button" type="button" id="${s}">${s}</button>`;g.insertAdjacentHTML("afterbegin",r)}function X(e){document.querySelectorAll(".page-button").forEach(r=>{if(r.id===e)switch(r.classList.add("active"),f){case"filter":i.page=e,K();break;case"workout":v=e;const s={limit:q(),page:v};s[l.content]=l.title,$(s);break}else r.classList.remove("active")})}function Z(){document.documentElement.clientWidth>=1440?(i.page=1,i.limit=12):(i.page=1,i.limit=8)}function ee(){L(),H(),n.innerHTML="",g.innerHTML=""}function te(e){let t="";e=Math.floor(e);for(let r=0;r<e;r++)t+=`
+      <button class="page-button" type="button" id="${s}">${s}</button>`;g.insertAdjacentHTML("afterbegin",r)}function X(e){document.querySelectorAll(".page-button").forEach(r=>{if(r.id===e)switch(r.classList.add("active"),f){case"filter":a.page=e,K();break;case"workout":v=e;const s={limit:q(),page:v};s[l.content]=l.title,$(s);break}else r.classList.remove("active")})}function Z(){document.documentElement.clientWidth>=1440?(a.page=1,a.limit=12):(a.page=1,a.limit=8)}function ee(){L(),H(),n.innerHTML="",g.innerHTML=""}function te(e){let t="";e=Math.floor(e);for(let r=0;r<e;r++)t+=`
         <span class="rating-star-icon">
             <svg class="rating-star" width="18" height="18" aria-label="rating-star">
                    <use href=${G}></use>
